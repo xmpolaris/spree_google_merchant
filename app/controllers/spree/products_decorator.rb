93aae99ea475734bdbb0aa044bde10fef2b365a8
Chen ZhongXue <xmpolaris@gmail.com>
2013-05-08 18:10:09 +0800

    add required fields

--- app/controllers/spree/products_decorator.rb
+++ app/controllers/spree/products_decorator.rb
@@ -1,5 +1,6 @@
 Spree::ProductsController.class_eval do
   def google_merchant
+    @taxons = Spree::Taxon.all.inject({}) {|a,b| a.merge({b.id.to_s => b.pretty_name})}
     @products = Spree::Product.joins(
       "LEFT JOIN `spree_variants` ON
         `spree_variants`.`product_id` = `spree_products`.`id` AND 
@@ -9,6 +10,9 @@ Spree::ProductsController.class_eval do
         `spree_assets`.`viewable_id` = `spree_variants`.`id` AND
         `spree_assets`.`type` = 'Spree::Image' AND
         `spree_assets`.`viewable_type` = 'Spree::Variant'"
+    ).joins(
+      "LEFT JOIN `spree_products_taxons` ON
+        `spree_products_taxons`.`product_id` = `spree_products`.`id`"
     ).select(
       'spree_products.id as id,
       spree_products.name as name,
@@ -16,7 +20,18 @@ Spree::ProductsController.class_eval do
       spree_products.description as description,
       spree_variants.sku as v_sku,
       spree_variants.price as v_price,
+      spree_variants.count_on_hand as v_count_on_hand,
+      spree_products_taxons.taxon_id as v_taxon_id,
       spree_assets.attachment_file_name as img_name'
-    ).group('spree_products.id').active
+    ).group('spree_products.id').active.limit(5)
+  end
+
+  protected
+  def google_merchant_product_type(product)
+    ancestor_chain = product.ancestors.inject("") do |name, ancestor|
+      name += "#{ancestor.name} > "
+    end
+    ancestor_chain + "#{name}"
   end
 end
+
--- app/views/spree/products/google_merchant.rss.builder
+++ app/views/spree/products/google_merchant.rss.builder
@@ -1,12 +1,14 @@
 xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
 
-xml.rss "version" => "2.0", "xmlns:g" => "http://base.google.com/ns/1.0" do
+xml.rss "version" => "2.0", "xmlns:g" => "http://base.google.com/ns/1.0", "encoding" => "UTF-8" do
   xml.channel do
     xml.title Spree::GoogleMerchant::Config[:google_merchant_title]
     xml.description Spree::GoogleMerchant::Config[:google_merchant_description]
 
     production_domain = Spree::GoogleMerchant::Config[:production_domain]
     xml.link production_domain
+
+    currency = Spree::Config.currency
 
     @products.each do |product|
       xml.item do
@@ -16,11 +18,14 @@ xml.rss "version" => "2.0", "xmlns:g" => "http://base.google.com/ns/1.0" do
         xml.link production_domain + 'products/' + product.permalink
         xml.tag! "g:mpn", product['v_sku'].to_s
         xml.tag! "g:id", product.id.to_s
-        xml.tag! "g:price", product['v_price'].to_s
+        xml.tag! "g:price", "#{product['v_price']} #{currency}"
         xml.tag! "g:condition", "new"
         xml.tag! "g:image_link", production_domain.sub(/\/$/, '') + "/spree/products/#{product.id}/product/#{product['img_name']}" unless product['img_name'].blank?
+        xml.tag! "g:availability", product['v_count_on_hand'] > 0 ? 'in stock' : 'out of stock'
+        xml.tag! "g:product_type", @taxons[product['v_taxon_id'].to_s]
       end
     end
   end
 end
+
 
